46e5a6f0c8c62894f5a4deeb1c93b5bad25acb45
andreluisrg <144398422+andreluisrg@users.noreply.github.com>
2024-03-20 15:57:38 -0300

    tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import AddLivro from "./pages/AddLivro"
 import CadAluno from "./pages/CadAluno"
 import { Home } from "./pages/Home"
+import LoginAluno from "./pages/LoginAluno"
 
 function App (){
 
   return (
     <>
       <div>
-        <AddLivro/>
+        <LoginAluno/>
       </div>
     </>
   )
