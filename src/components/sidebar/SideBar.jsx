d5ef82006d537d2737387c7d2bbf837136d4c3db
sandersonsamuel <sandersonsamuel401@gmail.com>
2024-05-16 17:51:43 -0300

    feat: ajuste na navbar para inserção de gerenciamento de cada entidade

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -2,14 +2,17 @@ import {SideBarItem} from "./SideBarItem.jsx";
 import {FaHistory, FaHome, FaUser} from "react-icons/fa";
 import { FaBook } from "react-icons/fa";
 import { FaHandshake } from "react-icons/fa";
-import {MdExitToApp, MdManageAccounts} from "react-icons/md";
+import {MdManageAccounts} from "react-icons/md";
 import {useState} from "react";
 import {Modal} from "react-responsive-modal";
 import {Button} from "../button/index.jsx";
+import bookIcon from "../../assets/book.svg";
 
 export const SideBar = ({children}) => {
 
     const [modalDesconectar, setModalDesconectar] = useState(false)
+
+    const [sidebarIsOpen, setSidebarIsOpen] = useState(false)
 
     const handleLogOut = () =>{
         localStorage.clear()
@@ -23,75 +26,64 @@ export const SideBar = ({children}) => {
             path: "/",
         },
         {
-            name: 'Cadastrar Aluno',
+            name: 'Aluno',
             icon: <FaUser />,
-            path: '/cadastro/aluno'
+            option: [
+                {name: 'Cadastrar Aluno', path: '/cadastro/aluno'},
+                {name: 'Gerenciar Aluno', path: '/gerenciar/aluno'}
+            ]
         },
         {
             name: 'Cadastrar Livro',
             icon: <FaBook/>,
-            path: "/cadastro/livro"
+            option: [
+                {name: 'Cadastrar Livro', path: '/cadastro/livro'},
+                {name: 'Gerenciar Livro', path: '/gerenciar/livro'}
+            ]
         },
         {
             name: 'Registrar Emprestimo',
             icon: <FaHandshake/>,
-            path: "/cadastro/emprestimo"
-        },
-        {
-            name: 'Gerenciar Empréstimos',
-            icon: <MdManageAccounts />,
-            path: "/gerenciar/emprestimo"
-        },
-        {
-            name: 'Historico Empréstimos',
-            icon: <FaHistory/>,
-            path: "/historico/emprestimo"
+            option: [
+                {name: 'Cadastrar Emprestimo', path: '/cadastro/emprestimo'},
+                {name: 'Gerenciar Emrpestimo', path: '/gerenciar/emprestimo'},
+                {name: 'Historico Empréstimos', path: '/historico/emprestimo'}
+            ]
         }
     ]
 
     return (
-        <div className="flex w-screen">
-            <div className={'xl:min-w-[350px] md:min-w-[280px] h-screen border-r-2 bg-slate-900 border-gray-200 flex flex-col'}>
+        <div className="flex">
 
-                <div className={'flex text-xl text-white items-center gap-3 p-5'}>
-                    <div className={'w-[50px] h-[50px] bg-white rounded-full text-black flex justify-center items-center font-bold'}>
-                        BA
+            <aside className={'h-screen bg-slate-900'}>
+
+                <nav className={'h-full flex flex-col'}>
+
+                    <div className={'flex px-3 py-2 gap-4 border-b border-slate-800'}>
+                        <img className={'w-10'} src={bookIcon} alt="icone 2 livros um encima do outro"/>
+                        <p className={'text-2xl font-semibold text-white'}>Biblioteca Azevedo</p>
                     </div>
-                    <p>
-                        {'Biblioteca Azevedo'}
-                    </p>
 
-                    <p className={'text-red-500 text-2xl cursor-pointer'} title={'Desconectar usuário'}>
-                        <MdExitToApp onClick={()=> setModalDesconectar(true)} />
-                    </p>
-
-                    <Modal open={modalDesconectar} onClose={()=> setModalDesconectar(false)}>
+                    <Modal open={modalDesconectar} onClose={() => setModalDesconectar(false)}>
                         <p className={'text-xl px-5 mr-5'}>Tem certeza que deseja se desconectar da aplicação?</p>
 
                         <div className={'flex gap-2 items-end'}>
-                            <Button onClick={()=> setModalDesconectar(false)}>Cancelar</Button>
+                            <Button onClick={() => setModalDesconectar(false)}>Cancelar</Button>
                             <Button onClick={handleLogOut}>Desconectar</Button>
                         </div>
                     </Modal>
 
-                </div>
-
-                <div>
-
-                    <div className={'bg-slate-950 w-full h-[50px] flex items-center text-slate-500 px-5'}>
-                        MENU DE NAVEGAÇÃO
-                    </div>
-
-                    <div>
+                    <div className={'text-sm'}>
                         {sideBarItems.map((item, index) => (
-                            <SideBarItem key={index} icon={item.icon} name={item.name} path={item.path}/>
+                            <SideBarItem key={index} icon={item.icon} name={!sidebarIsOpen && item.name} options={item.option}/>
                         ))}
                     </div>
 
-                </div>
+                </nav>
+            </aside>
 
-            </div>
             {children}
         </div>
     );
 };
+
--- src/components/sidebar/SideBarItem.jsx
+++ src/components/sidebar/SideBarItem.jsx
@@ -1,14 +1,32 @@
 import {Link, useLocation} from "react-router-dom";
 import classNames from "classnames";
+import {useState} from "react";
 
-export const SideBarItem = ({icon, name, path}) => {
+export const SideBarItem = ({icon, name, options}) => {
 
     const local = useLocation().pathname
+    const [showOpt, setShowOpt] = useState(false)
 
     return (
-        <Link to={path} className={classNames("w-full hover:bg-slate-800 flex p-4 items-center text-white gap-2 cursor-pointer", local == path && 'bg-slate-800' )}>
-            <p className={'text-2xl'}>{icon}</p>
-            {name.toUpperCase()}
-        </Link>
+        <div onClick={() => setShowOpt(!showOpt)} className={classNames("w-full flex flex-col justify-center text-white cursor-pointer hover:bg-slate-800", showOpt && 'bg-slate-800')}>
+
+            <div className={classNames('flex gap-3 items-center px-4 my-3')}>
+                <p className={'text-2xl'}>{icon}</p>
+                <p>{name}</p>
+            </div>
+
+            {
+                options &&
+
+                <div className={'flex flex-col bg-slate-800'}>
+                    {
+                        showOpt && options.map((item, index) => (
+                            <Link key={index} to={item.path}
+                                  className={classNames("py-3 border-b border-slate-600 px-4 hover:bg-slate-700")}>{item.name}</Link>
+                        ))
+                    }
+                </div>
+            }
+        </div>
     );
 };
