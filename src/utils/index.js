dd28eb36ab6ca54900a521320101f18262a8eae4
sandersonsamuel <sandersonsamuel401@gmail.com>
2024-06-11 01:09:19 -0300

    feat: tramento de erro de sessão expirada

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,8 +2,16 @@ import {livro} from "../../proxyState/index.js";
 import toast from "react-hot-toast";
 
 export const networkError = (e) => {
-        console.log(e)
-        toast.error("Network Error");
+        if (e.response.status === 401){
+                if (location.pathname !== '/login'){
+                        toast.error("Sua sessaão expirou, faça o login novamente!");
+                        setTimeout(()=>{
+                                location.href = "/login";
+                        }, [1000])
+                }
+        }else{
+                toast.error("Network Error");
+        }
 }
 
 export const resetLivro = () =>{
